a9ad7c0417e8b63e700f30f8092861e927536fcd
MelvinMuuo <muuomelvin73@gmail.com>
2019-04-27 05:38:44 +0300

    add delete button

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,9 +8,23 @@ import { Quote } from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes = [
-    new Quote('God is good!', 'Bible', 'Peter'),
-    new Quote('Work hard.', 'Me', 'Melvin'),
+    new Quote('God is good!', 'Bible', 'Peter', new Date(2018,3,14)),
+    new Quote('Work hard.', 'Me', 'Melvin', new Date(2018,5,6)),
   ]
+
+  DeleteQuote(isComplete,index){
+    if (isComplete){
+      let toDelete=confirm('Are you sure you want to delete $(this.quotes[index].quote)?')
+
+      if(toDelete) {
+        this.quotes.splice(index,1)
+      }
+    }
+  }
+
+  toogleDetails(index){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
 
   constructor() { }
 
