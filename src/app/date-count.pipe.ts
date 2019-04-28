f3ba4ad7df77d68ef9034adbd045036e73c5e4af
MelvinMuuo <muuomelvin73@gmail.com>
2019-04-28 18:41:30 +0300

    add custom pipe that shows how much time has passed since the quote was created

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -8,14 +8,14 @@ export class DateCountPipe implements PipeTransform {
   transform(value: any): number {
     let today:Date = new Date();
     let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
-    var dateDifference =Math.abs(value-todayWithNoTime )
+    var dateDifference =Math.abs(todayWithNoTime-value )
     const secondsInADay= 86400;
 
     var dateDifferenceSeconds=dateDifference*0.001;
 
     var dateCounter = dateDifferenceSeconds/secondsInADay;
 
-    if (dateCounter >= 1 && value > todayWithNoTime) {
+    if (todayWithNoTime > value) {
       return dateCounter;
 
     }else {
